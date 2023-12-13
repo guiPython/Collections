671b5400ed6c0ecf9742802e19b44e9eb4c89ed3
guiPython <guipython2001@gmail.com>
2023-12-13 10:27:22 -0300

    fix: Aula 03 testes de array list

--- Tests/ArrayListTest.cs
+++ Tests/ArrayListTest.cs
@@ -3,11 +3,18 @@ using Collections;
 namespace Tests;
 
 public class ArrayListTest {
+    [Fact(DisplayName = "Criando um ArrayList vazio")]
+    public void ShouldCreateAnEmptyLinkedList() {
+        var list = new ArrayList<int>(2);
+        Assert.Equal(0, list.Count);
+    }
+
     [Fact(DisplayName = "Adicionando elemento em um ArrayList vazio")]
     public void ShouldAddAnElementWhenArrayListIsEmpty() {
-        var emptyList = EmptyIntegerArrayList();
-        var added = emptyList.Add(10);
+        var list = EmptyIntegerArrayList();
+        var added = list.Add(10);
         Assert.True(added);
+        Assert.Equal(1, list.Count);
     }
 
     [Fact(DisplayName = "Adicionando elemento em um ArrayList cheio")]
@@ -15,6 +22,7 @@ public class ArrayListTest {
         var fullList = FullIntegerArrayList();
         var added = fullList.Add(10);
         Assert.True(added);
+        Assert.Equal(3, fullList.Count);
     }
 
     [Fact(DisplayName = "Removendo elemento existente do ArrayList")]
@@ -22,6 +30,7 @@ public class ArrayListTest {
         var binaryList = BinaryList();
         var removed = binaryList.Remove(0);
         Assert.True(removed);
+        Assert.Equal(1, binaryList.Count);
     }
 
     [Fact(DisplayName = "Removendo elemento que nao existente do ArrayList")]
@@ -29,13 +38,15 @@ public class ArrayListTest {
         var binaryList = BinaryList();
         var removed = binaryList.Remove(10);
         Assert.False(removed);
+        Assert.Equal(2, binaryList.Count);
     }
 
     [Fact(DisplayName = "Removendo elemento de um ArrayList vazio")]
     public void ShouldReturnFalseWhenRemoveFromEmptyList() {
-        var binaryList = EmptyIntegerArrayList();
-        var removed = binaryList.Remove(10);
+        var empyList = EmptyIntegerArrayList();
+        var removed = empyList.Remove(10);
         Assert.False(removed);
+        Assert.Equal(0, empyList.Count);
     }
 
     [Fact(DisplayName = "Verificando se o ArrayList contem um elemento")]
@@ -43,6 +54,7 @@ public class ArrayListTest {
         var binaryList = BinaryList();
         var contains = binaryList.Contains(0);
         Assert.True(contains);
+        Assert.Equal(2, binaryList.Count);
     }
 
     [Fact(DisplayName = "Verificando se o ArrayList nao contem um elemento")]
@@ -50,6 +62,7 @@ public class ArrayListTest {
         var binaryList = BinaryList();
         var contains = binaryList.Contains(3);
         Assert.False(contains);
+        Assert.Equal(2, binaryList.Count);
     }
 
     private static ArrayList<int> EmptyIntegerArrayList() => new(capacity: 2);
